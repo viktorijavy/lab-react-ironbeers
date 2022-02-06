032ef17d23bb5666b97c8529ff8600eee64eddeb
Viktorija Kuneviciute <viktorija.qun@gmail.com>
2022-02-06 17:47:14 +0100

    added search

--- src/pages/Beers.js
+++ src/pages/Beers.js
@@ -5,32 +5,45 @@ import BeerCard from '../components/BeerCard';
 
 const Beers = () => {
 
+    const [ query, setQuery ] = useState('')
+    const [ beers, setBeers ] = useState([]);
+    
+    const handleSearchChange = (e) => {
+      setQuery(e.target.value)
+    }
 
-    const [beers, setBeers] = useState([]);
+    
 
     useEffect(() => {
-        axios.get('https://ih-beers-api2.herokuapp.com/beers')
+        axios.get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${query}`)
             .then(response => {
                 console.log(response)
                 setBeers(response.data)
             })
             .catch(err => console.log(err))
-    }, [])
+    }, [query])
 
 
 
     return (
-        <div className='grid'>
 
-            {beers.map(beer => {
+        <>
+            <div className='searchfield'>
+                <input type="text" placeholder='search here' value={query} className="input-field" onChange={handleSearchChange} />
+            </div>
 
-                return <div key={beer._id}>
-                 <BeerCard beer={beer}/>
-                  </div>
-            
-            })}
+            <div className='grid'>
 
-        </div>
+                {beers.map(beer => {
+
+                    return <div key={beer._id}>
+                        <BeerCard beer={beer} />
+                    </div>
+
+                })}
+
+            </div>
+        </>
     )
 }
 
